62e803fecf2c93cc7043a1095e2b435600fc5b44
Vladimir Schöner <Wowtschikus@gmx.net>
2019-01-21 22:08:01 +0100

    interfaces added

---
+++ src/main/java/de/schoener/design/patterns/p05/chainOfResponsibility/v1/MainClass.java
@@ -0,0 +1,10 @@
+package de.schoener.design.patterns.p05.chainOfResponsibility.v1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/de/schoener/design/patterns/p05/chainOfResponsibility/v1/Seller.java
+++ src/main/java/de/schoener/design/patterns/p05/chainOfResponsibility/v1/Seller.java
@@ -1,5 +1,7 @@
 package de.schoener.design.patterns.p05.chainOfResponsibility.v1;
 
 public interface Seller {
+	boolean isResponsibleForProduct(Product product);
 
+	void sellProduct(Product product, Purchaser purchaser);
 }

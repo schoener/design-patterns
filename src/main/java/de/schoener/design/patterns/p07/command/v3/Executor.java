6ecf83c6d50fa6a82debbc580fdeffd08fae09d6
Vladimir Schöner <Wowtschikus@gmx.net>
2019-02-05 22:32:34 +0100

    version3 for command pattern

---
+++ src/main/java/de/schoener/design/patterns/p07/command/v3/Executor.java
@@ -0,0 +1,13 @@
+package de.schoener.design.patterns.p07.command.v3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Executor {
+	private final List<FileOperation> textFileOperations = new ArrayList<>();
+
+	public void executeOperation(FileOperation fileOperation) {
+		textFileOperations.add(fileOperation);
+		fileOperation.execute();
+	}
+}
--- src/main/java/de/schoener/design/patterns/p07/command/v3/TextFile.java
+++ src/main/java/de/schoener/design/patterns/p07/command/v3/TextFile.java
@@ -1,11 +1,17 @@
 package de.schoener.design.patterns.p07.command.v3;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TextFile {
+	private final Logger logger = LoggerFactory.getLogger(TextFile.class);
 
 	public void openFile() {
+		logger.info("About to open file");
 	}
 
 	public void saveFile() {
+		logger.info("About to save file");
 	}
 
 }
